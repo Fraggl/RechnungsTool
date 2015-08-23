1915ec60623ed3b490b6b9ba9bf0a1fd8d07f52c
Robert Wilhelm <wilhelmr@outlook.com>
2015-08-23 11:47:42 +0200

    + Standards_OpenFileDialog-logic

--- RechnungsTool/StandardsWindow.xaml.cs
+++ RechnungsTool/StandardsWindow.xaml.cs
@@ -23,5 +23,31 @@ namespace RechnungsTool
         {
             InitializeComponent();
         }
+
+        private void Standards_Speicherort_FileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text Files (*.txt)|*.txt";
+            
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                txtbox_standards_openFilePath.Text = filename;
+            }
+        }
     }
 }
