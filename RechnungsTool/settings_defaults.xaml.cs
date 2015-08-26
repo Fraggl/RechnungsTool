08c7f418afb97f46361f89d75cc595004a225d38
Fraggl Lappi <fraggi.robse@googlemail.com>
2015-08-26 02:18:15 +0200

    check

---
+++ RechnungsTool/settings_defaults.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RechnungsTool
+{
+    /// <summary>
+    /// Interaktionslogik für settings_defaults.xaml
+    /// </summary>
+    public partial class settings_defaults : Window
+    {
+        public settings_defaults()
+        {
+            InitializeComponent();
+        }
+    }
+}
